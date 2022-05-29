8297d63ca9f61b3ab0515a34c5c38abd7adbc8db
Xiong, Pin <pinxiongcn@foxmail.com>
2022-05-29 08:53:52 +0800

    feat: Add log level

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM golang:1.16.4-alpine3.13 AS build
+FROM golang:1.18.1-alpine3.15 AS build
 
 MAINTAINER "Xiong, Pin" "pinxiongcn@foxmail.com"
 
@@ -12,22 +12,3 @@ FROM alpine AS final
 COPY --from=build /bin/project/ /bin/project/
 EXPOSE 8080
 ENTRYPOINT ["/bin/project/golang-web"]
-
-
-#FROM golang:1.18.1
-#
-#MAINTAINER "Xiong, Pin" "pinxiongcn@foxmail.com"
-#
-#RUN apt-get update -y; \
-#    apt install -y build-essential; \
-#    apt-get install -y vim; \
-#    apt-get install -y gcc;
-#
-#WORKDIR /go/src/project/
-#COPY . .
-#RUN go mod download; \
-#    go mod verify; \
-#    go build -o /bin/project/golang-web src/main.go;
-#
-#EXPOSE 8080
-#ENTRYPOINT ["/bin/project/golang-web"]
--- src/main.go
+++ src/main.go
@@ -1,14 +1,35 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	log "github.com/sirupsen/logrus"
+)
+
+func init() {
+	log.SetLevel(log.InfoLevel)
+	log.SetFormatter(&log.JSONFormatter{
+		DisableTimestamp:  false,
+		DisableHTMLEscape: false,
+		FieldMap: log.FieldMap{
+			log.FieldKeyTime:  "timestamp",
+			log.FieldKeyLevel: "level",
+			log.FieldKeyMsg:   "message",
+			log.FieldKeyFunc:  "caller",
+		},
+		PrettyPrint: false,
+	})
+
+}
 
 func main() {
+	gin.SetMode(gin.ReleaseMode)
 	router := gin.New()
 	router.GET("/hello", func(context *gin.Context) {
 		user := context.Query("user")
 		context.JSON(200, gin.H{
 			"message": "Hello " + user,
 		})
+		log.Infof("Received a request! user:%v", user)
 	})
 	router.Run()
 }
